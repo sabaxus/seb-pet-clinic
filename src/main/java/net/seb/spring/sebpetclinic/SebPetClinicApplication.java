27e7be84d2c966eb20e1cc688c95864a41681b47
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-09-28 18:33:31 +0300

    Initial Project Add

---
+++ src/main/java/net/seb/spring/sebpetclinic/SebPetClinicApplication.java
@@ -0,0 +1,13 @@
+package net.seb.spring.sebpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/seb/spring/sebpetclinic/SebPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.seb.spring.sebpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
