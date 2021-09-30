e1adda90d85f47b734e28ec4744fe3514416de28
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-09-30 23:24:47 +0300

    Implement Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package net.seb.spring.sebpetclinic.services.map;
+
+import net.seb.spring.sebpetclinic.model.Owner;
+import net.seb.spring.sebpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
