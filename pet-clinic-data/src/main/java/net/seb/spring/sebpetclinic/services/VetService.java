ee2022d8de3b19416fcabf3030b226aa641aab32
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-09-30 22:49:22 +0300

    Refactoring to common Crud Interface. Closes #11.

--- pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package net.seb.spring.sebpetclinic.services;
 
 import net.seb.spring.sebpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
     Vet findByLastName(String lastName);
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
