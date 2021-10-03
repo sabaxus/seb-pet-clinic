687a8792cf8a6479aa738ce2e93118f53186facb
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-10-03 20:25:46 +0300

    Added Vets List and updated Controller. Closes #15.

--- pet-clinic-web/src/main/java/net/seb/spring/sebpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/net/seb/spring/sebpetclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package net.seb.spring.sebpetclinic.controllers;
 
+import net.seb.spring.sebpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets/index.htm"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets/index.htm"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
