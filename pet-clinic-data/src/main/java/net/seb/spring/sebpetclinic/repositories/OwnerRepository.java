d82f396b2f586f5db70e7dfdac11b67592389242
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-10-11 00:50:03 +0300

    Added Spring Data JPA Service for Owners. Closes #40.

--- pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import net.seb.spring.sebpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
