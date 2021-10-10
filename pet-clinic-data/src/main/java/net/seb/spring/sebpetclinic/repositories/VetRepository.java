4d13fce168146d95f827fa14ddda78bc365950a5
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-10-11 01:01:01 +0300

    Added Spring Data JPA Service for Vets. Closes #41.

--- pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/repositories/VetRepository.java
@@ -4,4 +4,5 @@ import net.seb.spring.sebpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VetRepository extends CrudRepository<Vet, Long> {
+    Vet findByLastName(String lastName);
 }
