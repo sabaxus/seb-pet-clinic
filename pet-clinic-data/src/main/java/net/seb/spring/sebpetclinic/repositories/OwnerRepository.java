7d8c2d6db20d05b9c25d1baee135202e79244190
Sebastian Ostafiev <sebastian.ostafiev@yahoo.com>
2021-11-04 09:47:49 +0200

    Implemented find owner. Closes #54.

--- pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package net.seb.spring.sebpetclinic.repositories;
 import net.seb.spring.sebpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/net/seb/spring/sebpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package net.seb.spring.sebpetclinic.services;
 
 import net.seb.spring.sebpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
